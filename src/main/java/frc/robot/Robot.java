2022affad11a79237798b941673d003969f20431
dibletexe <aaryan.porwal@gmail.com>
2026-09-07 18:20:13 -0500

    Added Needed Files

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -11,7 +11,7 @@ import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
 
 
-public class Robot extends TimedRobot
+public class Robot extends LoggedRobot
 {
     private Command autonomousCommand;
     
